d8f70f2c52a3223ea05efcb5359e358d10e1f072
expedito <116929658+expeditoernande@users.noreply.github.com>
2026-09-19 02:44:34 -0300

    Configurar deploy automático no GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import { CartProvider } from './context/CartContext'
 import { UIProvider } from './context/UIContext'
 import './index.css'
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, '') || '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <UIProvider>
         <CartProvider>
           <App />
